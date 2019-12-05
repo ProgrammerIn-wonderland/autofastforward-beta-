7f271cb7750abd83483f8fc218cc30a226077580
forthesafeofnealandfuller <30693865+forthesafeofnealandfuller@users.noreply.github.com>
2019-12-05 13:34:36 -0500

    Update Main.java

--- BUKKIT/src/com/mycompany/autofastforwardbeta/Main.java
+++ BUKKIT/src/com/mycompany/autofastforwardbeta/Main.java
@@ -1,63 +1,55 @@
 package com.mycompany.autofastforwardbeta;
 
-import org.bukkit.plugin.java.JavaPlugin;
 import dosse.upnp.UPnP;
-import org.bukkit.command.*;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin {
+public class Main
+  extends JavaPlugin {
+  public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    if (cmd.getName().equalsIgnoreCase("pf")) {
+      if (sender instanceof Player) {
+        Player player = (Player)sender;
+        
+        String port = String.join(" ", (CharSequence[])args);
+        
+        int intport = Integer.parseInt(port);
+        
+        UPnP.openPortUDP(intport);
+        UPnP.openPortTCP(intport);
+        player.sendMessage("Port " + intport + " forwarded.");
+        System.out.println("Port " + intport + " forwarded.");
+      } 
+
+      
+      return true;
+    }  if (cmd.getName().equalsIgnoreCase("unpf")) {
+      if (sender instanceof Player) {
+        Player player = (Player)sender;
+        
+        String port = String.join(" ", (CharSequence[])args);
+        
+        int intport = Integer.parseInt(port);
+        
+        UPnP.closePortUDP(intport);
+        UPnP.closePortTCP(intport);
+        player.sendMessage("Port " + intport + " unforwarded.");
+        System.out.println("Port " + intport + " unforwarded.");
+      } 
+      
+      return true;
+    } 
     
-    @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if (cmd.getName().equalsIgnoreCase("pf")) {
-            if (sender instanceof Player) {
-                Player player = (Player) sender;
-                //gets data of argument
-                String port = String.join(" ", args);
-                //convert string to int
-                int intport = Integer.parseInt(port);
-                //forwards port
-                UPnP.openPortUDP(intport);
-                UPnP.openPortTCP(intport);
-                System.out.println("Port " + intport + " forwarded.");
-            }
-
-        // If the player (or console) uses our command correct, we can return true
-        return true;
-        }else if(cmd.getName().equalsIgnoreCase("unpf")){
-            if (sender instanceof Player) {
-                Player player = (Player) sender;
-                //gets data of argument
-                String port = String.join(" ", args);
-                //convert string to int
-                int intport = Integer.parseInt(port);
-                //unforwards port
-                UPnP.closePortUDP(intport);
-                UPnP.closePortTCP(intport);
-                System.out.println("Port " + intport + " forwarded.");
-            }
-                // If the player (or console) uses our command correct, we can return true
-                return true;
-        }
-    // If the player (or console) uses our command correct, we can return false
     return false;
-    }
- 
-    @Override
-    public void onEnable(){
-        System.out.println("Forwarding......");
-        //forwards port
-        UPnP.openPortUDP(getServer().getPort());
-        UPnP.openPortTCP(getServer().getPort());
-        System.out.println("Port " + getServer().getPort() + " forwarded.");
-    }
- 
-    @Override
-    public void onDisable(){
-        System.out.println("unforwarding");
-        //unforwards port
-        UPnP.closePortUDP(getServer().getPort());
-        UPnP.closePortTCP(getServer().getPort());
-        System.out.println("Port " + getServer().getPort() + " unforwarded.");
-    }
-}
+  }
+
+  
+  public void onEnable() {
+    System.out.println("Forwarding......");
+    
+    UPnP.openPortUDP(getServer().getPort());
+    UPnP.openPortTCP(getServer().getPort());
+    System.out.println("Port " + getServer().getPort() + " forwarded.");
+  }
